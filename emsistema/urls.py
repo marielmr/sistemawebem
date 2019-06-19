8e08722402340a131f4f000ed6e3db69a6b62b5a
marielmr <marielmr@django.com>
2019-06-19 16:43:03 -0500

    urlmodificada

--- emsistema/urls.py
+++ emsistema/urls.py
@@ -3,9 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-<<<<<<< HEAD
 ]
-=======
-    #path('', views.login, name='login'),
-]
->>>>>>> 025258c966a074614e45653406544617ca13f6c2
