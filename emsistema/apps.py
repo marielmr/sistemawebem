7d40ca7add6293dd09a694e261e007bb566f06cb
marielmr <marielmr@django.com>
2019-06-19 12:29:27 -0500

    Sistema Web EM

---
+++ emsistema/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EmsistemaConfig(AppConfig):
+    name = 'emsistema'
---
+++ emsistema/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+# Create your models here.
+
+class person(models.Model):
+	idperson=models.AutoField(primary_key=True)
+	firstname=models.CharField(max_length=50)
+	lastname=models.CharField(max_length=50)
+	birthdate=models.DateField()
+
+class user(models.Model):
+	iduser=models.AutoField(primary_key=True)
+	user=models.CharField(max_length=20)
+	password=models.CharField(max_length=8)
+	idperson=models.ForeignKey(person,on_delete=models.CASCADE)
+
+
+class doctor(models.Model):
+	iddoctor=models.AutoField(primary_key=True)
+	speciality=models.CharField(max_length=30)
+	idperson=models.ForeignKey(person,on_delete=models.CASCADE)
+
+
+class patient(models.Model):
+	idpatient=models.AutoField(primary_key=True)
+	idperson=models.ForeignKey(person,on_delete=models.CASCADE)
+	weight=models.FloatField()
+	cutes=models.IntegerField()
+
+
+class medicalvisit(models.Model):
+	idmedicalvisit=models.AutoField(primary_key=True)
+	idperson=models.ForeignKey(person,on_delete=models.CASCADE)
+	idpatient=models.ForeignKey(patient,on_delete=models.CASCADE)
+	comentary=models.TextField(max_length=300)
+	date=models.DateField()
+
+class cut(models.Model):
+	idcut=models.AutoField(primary_key=True)
+	idmedicalvisit=models.ForeignKey(medicalvisit,on_delete=models.CASCADE)
+	url=models.URLField()
+	numbercut=models.IntegerField()
+
+		
+		
+		
